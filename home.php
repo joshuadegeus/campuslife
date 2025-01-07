c09f1f8d8ee9c6b532a7c971bf9669560daee34a
joshuadegeus <joshuadegeus@unr.edu>
2025-01-07 08:46:40 -0800

    Delete home.php

--- home.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_GET['campus'])) {
-    $campus_id = htmlspecialchars($_GET['campus']);
-    echo "Selected Campus ID: " . $campus_id;
-} else {
-    echo "No campus selected.";
-}
-?>
